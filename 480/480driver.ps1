7b19bf48ecd4f4ab11b615128caac3a0fda81bf4
max-gallagher <70865313+max-gallagher@users.noreply.github.com>
2024-04-16 19:40:41 -0400

    Add files via upload

--- 480/480driver.ps1
+++ 480/480driver.ps1
@@ -8,9 +8,9 @@ $selected_vm = Select-VM -folder "PROD"
 $selected_snapshot = select_snapshot -vm $selected_vm
 $selected_datastore = select_datastore
 $selected_host = select_host
-#New-LinkedClone -VMName $selected_vm -SnapshotName $selected_snapshot -NewVMName $NewVMName -Datastore $selected_datastore -VMHost $selected_host
+New-LinkedClone -VMName $selected_vm -SnapshotName $selected_snapshot -NewVMName $NewVMName -Datastore $selected_datastore -VMHost $selected_host
 #New-Network -VMHost $selected_host
-Get-IP -VMName $selected_vm
-Set-Network -VMName $selected_vm
-PowerVM -VMName $selected_vm
+#Get-IP -VMName $selected_vm
+#Set-Network -VMName $selected_vm
+#PowerVM -VMName $selected_vm
 #480Disconnect
